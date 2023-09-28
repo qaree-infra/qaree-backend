95176e30076edc0f8e599a0d32376b1729d1f5e5
MOHAMED ALI FAWZI <76850143+MohamedAli00949@users.noreply.github.com>
2023-09-28 18:02:33 +0300

    fix: validateing email and otp at validate reset password otp mutation

--- src/graphql/user/validateResetPasswordOTP/resolve.ts
+++ src/graphql/user/validateResetPasswordOTP/resolve.ts
@@ -1,9 +1,19 @@
 import User from "../../../models/user.js";
 import OTPCode from "../../../models/otpcode.js";
+import { validateEmail } from "../../../utils/helper.js";
 
-const validateResetPasswordOTP = async (parent, {otp, email}) => {
-  try {
-    const user = await User.findOne({ email });
+const validateResetPasswordOTP = async (parent, { otp, email }) => {
+	try {
+		if (!email && !otp)
+			throw new Error("Invalid arguments, please enter the required data.");
+
+		if (!email) throw new Error("The email is required");
+		if (!otp) throw new Error("The otp is required");
+
+		if (!validateEmail(email)) throw new Error("Invalid email");
+		if (Number.isNaN(Number(otp))) throw new Error("Invalid otp");
+
+		const user = await User.findOne({ email });
 
 		if (!user) throw new Error("User not found.");
 
@@ -21,9 +31,9 @@ const validateResetPasswordOTP = async (parent, {otp, email}) => {
 			message: "Your OTP is successful for reset password",
 			success: true,
 		};
-  } catch (error) {
-    throw new Error(error);
-  }
-}
+	} catch (error) {
+		throw new Error(error);
+	}
+};
 
 export default validateResetPasswordOTP;
