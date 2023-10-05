4af840994c5c56087bd9f17c1f713e819b1f728a
MOHAMED ALI FAWZI <76850143+MohamedAli00949@users.noreply.github.com>
2023-10-05 22:59:40 +0300

    translate a validate reset password otp mutation messages

--- src/graphql/user/validateResetPasswordOTP/resolve.ts
+++ src/graphql/user/validateResetPasswordOTP/resolve.ts
@@ -1,22 +1,43 @@
 import User from "../../../models/user.js";
 import OTPCode from "../../../models/otpcode.js";
-import ResetId from '../../../models/resetId.js';
-import { validateEmail, createPasswordResetToken } from "../../../utils/helper.js";
+import ResetId from "../../../models/resetId.js";
+import {
+	validateEmail,
+	createPasswordResetToken,
+} from "../../../utils/helper.js";
 
-const validateResetPasswordOTP = async (parent, { otp, email }) => {
+const validateResetPasswordOTP = async (parent, { otp, email }, context) => {
+	const { lang } = context.query;
 	try {
 		if (!email && !otp)
-			throw new Error("Invalid arguments, please enter the required data.");
+			throw new Error(
+				lang === "ar"
+					? "الوسائط غير صالحة، يرجى إدخال البيانات المطلوبة."
+					: "Invalid arguments, please enter the required data.",
+			);
 
-		if (!email) throw new Error("The email is required");
-		if (!otp) throw new Error("The otp is required");
+		if (!email)
+			throw new Error(
+				lang === "ar" ? "البريد الإلكتروني مطلوب" : "The email is required",
+			);
 
-		if (!validateEmail(email)) throw new Error("Invalid email");
-		if (Number.isNaN(Number(otp))) throw new Error("Invalid otp");
+		if (!otp)
+			throw new Error(lang === "ar" ? "الكود مطلوب" : "The otp is required");
+
+		if (!validateEmail(email))
+			throw new Error(lang === "ar" ? "بريد إلكتروني خاطئ" : "Invalid email");
+
+		if (Number.isNaN(Number(otp)))
+			throw new Error(
+				lang === "ar" ? "كلمة مرور لمرة واحدة غير صالحة" : "Invalid otp",
+			);
 
 		const user = await User.findOne({ email });
 
-		if (!user) throw new Error("User not found.");
+		if (!user)
+			throw new Error(
+				lang === "ar" ? "هذا المستخدم غير موجود" : "User not found.",
+			);
 
 		const otpCode = await OTPCode.findOne({
 			userId: user._id,
@@ -24,18 +45,28 @@ const validateResetPasswordOTP = async (parent, { otp, email }) => {
 			type: "reset-password",
 		});
 
-		if (!otpCode) throw new Error("Sorry, invalid otp code");
+		if (!otpCode)
+			throw new Error(
+				lang === "ar" ? "اسف هذا الكود غير صالح" : "Sorry, invalid otp code",
+			);
 
 		await OTPCode.deleteOne({ _id: otpCode._id });
 
 		const token = createPasswordResetToken({ id: user._id }).split(".");
 
-		const createdToken = await ResetId.create({ partOne: token[0], partTwo: token[1], partThree: token[2] });
+		const createdToken = await ResetId.create({
+			partOne: token[0],
+			partTwo: token[1],
+			partThree: token[2],
+		});
 
 		return {
-			message: "Your OTP is successful for reset password",
+			message:
+				lang === "ar"
+					? "لقد نجح OTP الخاص بك في إعادة تعيين كلمة المرور"
+					: "Your OTP is successful for reset password",
 			success: true,
-			reset_token: createdToken.partThree
+			reset_token: createdToken.partThree,
 		};
 	} catch (error) {
 		throw new Error(error);
