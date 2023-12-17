0bed4d1f58af342af67f1581ae00864a92a1c2c1
Mido <=>
2023-12-17 22:44:30 +0200

    add admin signin mutation

--- src/graphql/mutation-type.ts
+++ src/graphql/mutation-type.ts
@@ -1,6 +1,7 @@
-import { GraphQLObjectType, GraphQLString } from "graphql";
+import { GraphQLObjectType } from "graphql";
 import UserMutation from "./user/user-mutation.js";
 import BookMutations from "./book/book-mutations.js";
+import AdminMutations from "./admin/admin-mutations.js";
 
 const mutationType = new GraphQLObjectType({
 	name: "Mutation",
@@ -8,6 +9,7 @@ const mutationType = new GraphQLObjectType({
 	fields: {
 		...UserMutation,
 		...BookMutations,
+		...AdminMutations,
 	},
 });
 
