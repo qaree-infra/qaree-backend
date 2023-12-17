0d555ed3300527d27c00117385a3160be7f5c556
Mido <=>
2023-12-17 17:36:08 +0200

    feat: add publish book mutation

---
+++ src/graphql/book/publishBook/mutation.ts
@@ -0,0 +1,29 @@
+import { GraphQLObjectType, GraphQLString, GraphQLBoolean } from "graphql"
+import myBook from '../myBook-type.js';
+import publishBookResolve from './resolve.js'
+
+const publishBookToType = new GraphQLObjectType({
+  name: "PublishBook",
+  description: "this is the publish book type",
+  fields: {
+    message: {
+      type: GraphQLString,
+      description: "",
+    },
+    book: {
+      type: myBook,
+      description: ""
+    },
+  }
+})
+
+export default {
+  type: publishBookToType,
+  args: {
+    bookId: {
+      type: GraphQLString,
+      description: "",
+    }
+  },
+  resolve: publishBookResolve,
+}
