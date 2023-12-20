56a969c897f1f9b4b7bcb5234e2fed2b3d0f573c
Mido <=>
2023-12-20 20:57:44 +0200

    feat: add get books query

--- src/graphql/book/book-queries.ts
+++ src/graphql/book/book-queries.ts
@@ -1,7 +1,9 @@
 import getUserBooks from "./user/getUserBooks/query.js";
 import getUserBook from './user/getUserBook/query.js';
+import getBooksResolve from "./user/getBooks/query.js";
 
 export default {
   getUserBook: getUserBook,
   getUserBooks: getUserBooks,
+  getBooks: getBooksResolve
 }
---
+++ src/graphql/book/book-type.ts
@@ -0,0 +1,82 @@
+import {
+	GraphQLObjectType,
+	GraphQLID,
+	GraphQLString,
+	GraphQLBoolean,
+	GraphQLInt,
+	GraphQLList,
+	GraphQLFloat,
+} from "graphql";
+import { authorType, fileType } from "./myBook-type.js";
+
+const bookInfoType = new GraphQLObjectType({
+	name: "BookInfo",
+	description: "This book datails",
+	fields: {
+		_id: {
+			type: GraphQLID,
+		},
+		name: {
+			type: GraphQLString,
+			description: "",
+		},
+		description: {
+			type: GraphQLString,
+			description: "",
+		},
+		isbn: {
+			type: GraphQLString,
+			description: "",
+		},
+		edition: {
+			type: GraphQLInt,
+			description: "",
+		},
+		publishingRights: {
+			type: GraphQLBoolean,
+			description: "",
+		},
+		categories: {
+			type: new GraphQLList(GraphQLString),
+			description: "",
+		},
+		avgRate: {
+			type: GraphQLInt,
+			description: "",
+		},
+		price: {
+			type: GraphQLFloat,
+			description: "",
+		},
+		language: {
+			type: GraphQLString,
+			description: "",
+		},
+		author: {
+			type: authorType,
+			description: "",
+		},
+		sample: {
+			type: fileType,
+			description: "",
+		},
+		cover: {
+			type: fileType,
+			description: "",
+		},
+		createdAt: {
+			type: GraphQLString,
+			description: "",
+		},
+		updatedAt: {
+			type: GraphQLString,
+			description: "",
+		},
+		publishionDate: {
+			type: GraphQLString,
+			description: "",
+		},
+	},
+});
+
+export default bookInfoType;
