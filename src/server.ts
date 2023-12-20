43a877b604ca2fe03f12fceb81ae0779ee640a9a
Mido <=>
2023-12-20 11:09:57 +0200

    feat: add admin auth

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import { graphqlHTTP } from "express-graphql";
 import schema from "./graphql/graphql-schema.js";
 import auth from "./middleware/auth.js";
 import uploadRoute from "./upload/route.js";
+import adminAuth from "./middleware/adminAuth.js";
 
 const app: express.Application = express();
 
@@ -14,6 +15,8 @@ app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(cors());
 
 app.use(auth);
+
+app.use(adminAuth);
 
 // app.get("/", (req: express.Request, res: express.Response) => {
 // 	res.send("Hello, world!");
