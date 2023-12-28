c325ab9d5cc82ee16fcdfc460f5bb2707f1bf2f1
MohamedAli00949 <=>
2023-12-28 09:25:49 +0200

    feat: add get books from recycle bin query

--- src/graphql/book/book-queries.ts
+++ src/graphql/book/book-queries.ts
@@ -4,6 +4,7 @@ import getBooks from "./user/getBooks/query.js";
 import getBook from "./user/getBookInfo/query.js";
 import getBookReviews from "./user/getBookReviews/query.js";
 import getBookReview from "./user/getBookReview/query.js";
+import getBooksFromRecycleBin from "./user/getBooksFromRecycleBin/query.js";
 
 export default {
   getUserBook: getUserBook,
@@ -12,4 +13,5 @@ export default {
   getBook: getBook,
   getBookReviews: getBookReviews,
   getBookReview: getBookReview,
+  getBooksFromRecycleBin: getBooksFromRecycleBin,
 }
