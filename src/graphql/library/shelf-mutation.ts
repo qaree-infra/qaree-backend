6071706cf66a77f255dbd8c53dbe65f66ef08ce9
MohamedAli00949 <=>
2023-12-29 22:31:14 +0200

    feat: add removeShelf mutation

--- src/graphql/library/shelf-mutation.ts
+++ src/graphql/library/shelf-mutation.ts
@@ -1,9 +1,11 @@
 import createShelf from "./createShelf/mutation.js";
 import addBookToShelf from "./addBookToShelf/mutation.js";
 import removeBookFromShelf from "./removeBookFromShelf/mutation.js";
+import removeShelf from "./removeShelf/mutation.js";
 
 export default {
 	createShelf,
 	addBookToShelf,
 	removeBookFromShelf,
+	removeShelf,
 };
