ce1f9893a913847ca0833758be566266800f9194
MohamedAli00949 <=>
2023-12-29 20:57:56 +0200

    feat: add getLibrary query

--- src/graphql/library/shelf-queries.ts
+++ src/graphql/library/shelf-queries.ts
@@ -1,5 +1,7 @@
 import getShelf from "./getShelf/query.js";
+import getLibrary from "./getLibrary/query.js";
 
 export default {
-  getShelf: getShelf,
-}
+	getShelf: getShelf,
+	getLibrary: getLibrary,
+};
