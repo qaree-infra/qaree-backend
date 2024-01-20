fea718d3896b958ac0c4a3cb02a72c9ed34178f4
MOHAMED ALI FAWZI <76850143+MohamedAli00949@users.noreply.github.com>
2024-01-21 00:20:10 +0200

    feat: create category model

---
+++ src/models/category.ts
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+interface CategoryInterface {
+	_id: string;
+	name_ar: string;
+	name_en: string;
+	icon: mongoose.Schema.Types.ObjectId;
+	adminId: string;
+	createdAt: Date;
+	updatedAt: Date;
+}
+
+const categorySchema = new mongoose.Schema<CategoryInterface>(
+	{
+		name_ar: {
+			type: String,
+			require: [true, "please enter the category name in arabic"],
+			trim: true,
+		},
+		name_en: {
+			type: String,
+			require: [true, "please enter the category name in english"],
+			trim: true,
+		},
+		adminId: {
+			type: String,
+			require: [true, "please enter the adminId"],
+			trim: true,
+		},
+		icon: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "File",
+		},
+	},
+	{
+		timestamps: true,
+	},
+);
+
+const Category = mongoose.model<CategoryInterface>("File", categorySchema);
+
+export default Category;
