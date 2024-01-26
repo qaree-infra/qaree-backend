3a9cf06ecdcc14a3d0317a83b5b84d1d0221513b
MOHAMED ALI FAWZI <76850143+MohamedAli00949@users.noreply.github.com>
2024-01-26 08:56:11 +0200

    feat: create admin delete account mutation

--- src/graphql/mutations/admin/admin-mutations.ts
+++ src/graphql/mutations/admin/admin-mutations.ts
@@ -1,9 +1,11 @@
-import adminSignInMutation from './signIn/mutation.js'
+import adminSignIn from './signIn/mutation.js'
 import adminRegister from './register/mutation.js'
 import adminUpdateAccount from './updateAccount/mutation.js'
+import adminDeleteAccount from './deleteAccount/mutation.js';
 
 export default {
-  adminSignIn: adminSignInMutation,
-  adminRegister: adminRegister,
-  adminUpdateAccount: adminUpdateAccount,
+  adminSignIn,
+  adminRegister,
+  adminUpdateAccount,
+  adminDeleteAccount
 }
