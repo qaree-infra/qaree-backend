8cb88471f5ef9e4b3581dccd1bd57d52f3984122
MOHAMED ALI FAWZI <76850143+MohamedAli00949@users.noreply.github.com>
2024-01-29 02:27:03 +0200

    feat: add get all users query

---
+++ src/graphql/queries/user/user/getUserInfo/resolve.ts
@@ -0,0 +1,18 @@
+import { auth } from "../../../../../middleware/auth.js";
+import User from "../../../../../models/user.js";
+
+const userInfo = async (_, args, context) => {
+	try {
+		const auth: auth = context.auth;
+
+		if (auth?.error) throw new Error(auth?.error);
+
+		const user = await User.findById(auth.user._id).populate('avatar');
+
+		return user;
+	} catch (error) {
+		throw new Error(error);
+	}
+};
+
+export default userInfo;
