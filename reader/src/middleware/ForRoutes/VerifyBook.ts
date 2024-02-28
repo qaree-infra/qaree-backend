e7d289e8f7a0724c9652b1c7b77ac8a0ac084cc5
MOHAMED ALI FAWZI <76850143+MohamedAli00949@users.noreply.github.com>
2024-02-28 17:34:16 +0200

    refactor(reader): remove verify book middlewares for author and admin

--- reader/src/middleware/ForRoutes/VerifyBook.ts
+++ reader/src/middleware/ForRoutes/VerifyBook.ts
@@ -2,15 +2,13 @@ import { Response, NextFunction } from "express";
 import { BookInterface } from "../../models/book.js";
 import { AuthRequest } from "../general/auth.js";
 import verifyBook from "../general/verifyBook.js";
-import verifyBookAuthor from "../general/verifyBookAuthor.js";
-import adminVerifyBook from "../general/adminVerifyBook.js";
 
-interface VerifyBook extends AuthRequest {
+interface VerifyBookRequest extends AuthRequest {
 	bookData: BookInterface;
 }
 
-export const VerifyBook = async (
-	req: VerifyBook,
+const VerifyBook = async (
+	req: VerifyBookRequest,
 	res: Response,
 	next: NextFunction,
 ) => {
@@ -35,60 +33,4 @@ export const VerifyBook = async (
 	}
 };
 
-export const VerifyBookAuthor = async (
-	req: VerifyBook,
-	res: Response,
-	next: NextFunction,
-) => {
-	try {
-		const { id } = req.params;
-		const { lang } = req.query;
-
-		const {
-			error,
-			bookData,
-			statusCode,
-		}: { error?: string; bookData?: BookInterface; statusCode: number } =
-			await verifyBookAuthor(
-				{ query: { lang: lang === "ar" ? "ar" : "en" } },
-				id,
-				req.auth.user._id,
-			);
-
-		if (error) return res.status(statusCode).json({ message: error });
-
-		req.bookData = bookData;
-
-		next();
-	} catch (error) {
-		res.status(500).json({ message: error.message });
-	}
-};
-
-export const VerifyBookAdmin = async (
-	req: VerifyBook,
-	res: Response,
-	next: NextFunction,
-) => {
-	try {
-		const { id } = req.params;
-		const { lang } = req.query;
-
-		const {
-			error,
-			bookData,
-			statusCode,
-		}: { error?: string; bookData?: BookInterface; statusCode: number } =
-			await adminVerifyBook(id, {
-				query: { lang: lang === "ar" ? "ar" : "en" },
-			});
-
-		if (error) return res.status(statusCode).json({ message: error });
-
-		req.bookData = bookData;
-
-		next();
-	} catch (error) {
-		res.status(500).json({ message: error.message });
-	}
-};
+export default VerifyBook;
