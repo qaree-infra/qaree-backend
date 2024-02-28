7adeca9cba99b5cd768b1079472f136d0159176a
MOHAMED ALI FAWZI <76850143+MohamedAli00949@users.noreply.github.com>
2024-02-28 15:35:51 +0200

    refactor(author): remove validate category middleware

--- author/src/graphql/queries/book/book-queries.ts
+++ author/src/graphql/queries/book/book-queries.ts
@@ -3,7 +3,6 @@ import getBookInfo from "./getBookInfo/query.js";
 import getUserBooks from "./getUserBooks/query.js";
 import getBook from "./getBookInfo/query.js";
 import getBooksFromRecycleBin from "./getBooksFromRecycleBin/query.js";
-import search from "./search/query.js";
 import getBookEPubMetadata from "./fileContent/getBookMetadata/query.js";
 import getBookEPubContent from "./fileContent/getBookEPubContent/query.js";
 import getBookEPubManifest from "./fileContent/getBookEPubManifest/query.js";
@@ -13,7 +12,6 @@ export default {
 	getMyBooks: getMyBooks,
 	getBooks: getUserBooks,
 	getBook: getBook,
-	search: search,
 	getBooksFromRecycleBin: getBooksFromRecycleBin,
 	getBookEPubMetadata: getBookEPubMetadata,
 	getBookEPubManifest: getBookEPubManifest,
