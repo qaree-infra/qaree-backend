4f2d2d1f1c8c58ea2f3b0ccdbef06efc6d1c7ec5
MOHAMED ALI FAWZI <76850143+MohamedAli00949@users.noreply.github.com>
2024-02-28 03:39:43 +0200

    refactor(author): remove admin and it's auth middleware

--- author/src/graphql/queries/admin/getAdminInfo/query.ts
+++
@@ -1,8 +0,0 @@
-import userType from "../../../types/admin-type.js";
-import resolve from './resolve.js';
-
-export default {
-  type: userType,
-  description: "This is the admin info",
-  resolve,
-};
