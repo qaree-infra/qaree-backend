eeecbfa71a344287811457d6e8659af104ac8eca
MOHAMED ALI FAWZI <76850143+MohamedAli00949@users.noreply.github.com>
2024-04-13 09:51:00 +0200

    fix(reader): change the room query

--- reader/src/chat/typeing.ts
+++ reader/src/chat/typeing.ts
@@ -1,17 +1,79 @@
+import mongoose from "mongoose";
+import Room from "../models/chatRoom.js";
+
 export default (io, socket) => {
-	return function isTypeing({ room, isTypeing }) {
+	return async function isTypeing({ room, isTypeing }) {
 		const userData = socket.handshake["authData"].user;
 		if (isTypeing !== false && isTypeing !== true) {
-			console.log("invalid typeing");
+			// console.log("invalid typeing");
 			return socket.emit("error", "invalid is typeing");
 		}
 
-		console.log(room, isTypeing);
+		if (!room || room.trim().length === 0)
+			return socket.emit("error", "Invalid room ID format");
 
-		if (socket.handshake["authData"].user.rooms.includes(room)) {
-			io.in(room).emit("typeing", {
-				userId: userData._id,
+		if (room.split("-").length !== 2) {
+			if (!mongoose.isObjectIdOrHexString(room))
+				return socket.emit("error", "Invalid room ID format");
+		}
+
+		// console.log(room, isTypeing);
+		const orOptions =
+			room.split("-").length !== 2
+				? [
+						{
+							roomId: room,
+							$or: [
+								{ creator: userData._id },
+								{ members: { $in: [userData._id] } },
+							],
+							activation: true,
+						},
+						{
+							_id: room,
+							$or: [
+								{ creator: userData._id },
+								{ members: { $in: [userData._id] } },
+							],
+							activation: true,
+						},
+						{
+							book: room,
+							members: { $in: [userData._id] },
+							activation: true,
+						},
+				  ]
+				: [
+						{
+							roomId: room,
+							creator: userData._id,
+							partner: room.split("-")[1],
+							activation: true,
+						},
+				  ];
+
+		const roomData = await Room.findOne({
+			$or: orOptions,
+		});
+		// console.log(roomData);
+
+		if (
+			roomData?.members.length > 0 &&
+			!roomData?.members?.includes(userData._id)
+		) {
+			return socket.emit("error", "invalid room");
+		}
+
+		if (roomData) {
+			io.in(roomData.roomId).emit("typeing", {
+				user: {
+					_id: userData._id,
+					name: userData.name,
+					avatar: userData.avatar,
+					bio: userData.bio,
+				},
 				room: room,
+				roomData: roomData,
 				isTypeing: isTypeing,
 			});
 		} else return socket.emit("error", "invalid room");
