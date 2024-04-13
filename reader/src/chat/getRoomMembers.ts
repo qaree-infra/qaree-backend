e80eb382a832393892b75494046a7af0b8e5f02b
MOHAMED ALI FAWZI <76850143+MohamedAli00949@users.noreply.github.com>
2024-04-13 10:33:48 +0200

    fix(reader): change the room query at chat room members event emitter

--- reader/src/chat/getRoomMembers.ts
+++ reader/src/chat/getRoomMembers.ts
@@ -4,20 +4,35 @@ export default (io, socket) => {
 	return async ({ room, limitMembers, page }) => {
 		const userData = socket.handshake["authData"].user;
 
-		const orOptions = [
-			{
-				_id: room,
-				activation: true,
-			},
-			{
-				roomId: room,
-				activation: true,
-			},
-			{
-				book: room,
-				activation: true,
-			},
-		];
+		const orOptions =
+			room.split("-").length !== 2
+				? [
+						{
+							roomId: room,
+							$or: [
+								{ creator: userData._id },
+								{ members: { $in: [userData._id] } },
+							],
+						},
+						{
+							_id: room,
+							$or: [
+								{ creator: userData._id },
+								{ members: { $in: [userData._id] } },
+							],
+						},
+						{
+							book: room,
+							members: { $in: [userData._id] },
+						},
+				  ]
+				: [
+						{
+							roomId: room,
+							creator: userData._id,
+							partner: room.split("-")[1],
+						},
+				  ];
 
 		const roomData: RoomInterface = await Room.findOne({
 			$or: orOptions,
@@ -25,7 +40,8 @@ export default (io, socket) => {
 
 		if (roomData) {
 			const currentPage = Number(page || 1);
-			const startIndex = (Number(currentPage) - 1) * (Number(limitMembers) || 10);
+			const startIndex =
+				(Number(currentPage) - 1) * (Number(limitMembers) || 10);
 			const roomMembers = await Room.findById(roomData._id).populate([
 				{
 					path: "members",
