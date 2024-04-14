17f66d87ef94a64ca685d78c82cd291efb76e257
MOHAMED ALI FAWZI <76850143+MohamedAli00949@users.noreply.github.com>
2024-04-14 15:32:48 +0200

    fix(reader): fix rooms population at get rooms event

--- reader/src/chat/getRooms.ts
+++ reader/src/chat/getRooms.ts
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import Room from "../models/chatRoom.js";
 
 export default (io, socket) => {
@@ -5,6 +6,7 @@ export default (io, socket) => {
 		const userData = socket.handshake["authData"].user;
 
 		const startIndex = (Number(page || 1) - 1) * limit;
+		console.log(keyword);
 		const keys = keyword
 			?.trim()
 			?.split(" ")
@@ -26,80 +28,182 @@ export default (io, socket) => {
 			},
 		];
 
-		const roomData = await Room.find({
-			$or: orOptions,
-		})
-			.limit(limit || 10)
-			.skip(startIndex)
-			.sort({ updatedAt: -1 })
-			.populate([
-				{
-					path: "partner",
-					match: { name: { $in: keys } },
-					options: {
-						select: "name avatar",
-						populate: { path: "avatar" },
-					},
+		const roomData = await Room.aggregate([
+			{
+				$lookup: {
+					from: "users", // Foreign collection for partner
+					localField: "partner", // Local field in Room schema (ObjectId)
+					foreignField: "_id", // Foreign field in User schema (ObjectId)
+					as: "partner",
 				},
-				{
-					path: "book",
-					match: { name: { $in: keys } },
-					options: {
-						select: "name cover",
-						populate: { path: "cover" },
-					},
+			},
+			{
+				$unwind: {
+					path: "$partner",
+					preserveNullAndEmptyArrays: true,
 				},
-			]);
+			},
+			{
+				$lookup: {
+					from: "files", // Foreign collection for avatar
+					localField: "partner.avatar", // Local field in User schema (ObjectId)
+					foreignField: "_id", // Foreign field in File schema (ObjectId)
+					as: "partner.avatar",
+				},
+			},
+			{
+				$unwind: {
+					path: "$partner.avatar",
+					preserveNullAndEmptyArrays: true,
+				},
+			},
+			{
+				$lookup: {
+					from: "books", // Foreign collection for book
+					localField: "book", // Local field in Room schema (ObjectId)
+					foreignField: "_id", // Foreign field in Book schema (ObjectId)
+					as: "book",
+				},
+			},
+			{
+				$unwind: {
+					path: "$book",
+					preserveNullAndEmptyArrays: true,
+				},
+			},
+			{
+				$lookup: {
+					from: "files", // Foreign collection for avatar
+					localField: "book.cover", // Local field in User schema (ObjectId)
+					foreignField: "_id", // Foreign field in File schema (ObjectId)
+					as: "book.cover",
+				},
+			},
+			{
+				$unwind: {
+					path: "$book.cover",
+					preserveNullAndEmptyArrays: true,
+				},
+			},
+			{
+				$lookup: {
+					from: "messages", // Foreign collection for book
+					localField: "lastMessage", // Local field in Room schema (ObjectId)
+					foreignField: "_id", // Foreign field in Book schema (ObjectId)
+					as: "lastMessage",
+				},
+			},
+			{
+				$unwind: {
+					path: "$lastMessage",
+					preserveNullAndEmptyArrays: true,
+				},
+			},
+			{
+				$lookup: {
+					from: "users", // Foreign collection for avatar
+					localField: "lastMessage.sender", // Local field in User schema (ObjectId)
+					foreignField: "_id", // Foreign field in File schema (ObjectId)
+					as: "lastMessage.sender",
+				},
+			},
+			{
+				$unwind: {
+					path: "$lastMessage.sender",
+					preserveNullAndEmptyArrays: true,
+				},
+			},
+			{
+				$lookup: {
+					from: "files", // Foreign collection for avatar
+					localField: "lastMessage.sender.avatar", // Local field in User schema (ObjectId)
+					foreignField: "_id", // Foreign field in File schema (ObjectId)
+					as: "lastMessage.sender.avatar",
+				},
+			},
+			{
+				$unwind: {
+					path: "$lastMessage.sender.avatar",
+					preserveNullAndEmptyArrays: true,
+				},
+			},
+			{
+				$match: {
+					$or: [
+						{
+							"partner.name": { $in: keys },
+							creator: userData._id,
+							activation: true,
+						},
+						{
+							"book.name": { $in: keys },
+							members: { $in: [userData._id] },
+							activation: true,
+						},
+						{
+							"lastMessage.content": { $in: keys },
+							$or: [
+								{
+									creator: userData._id,
+									activation: true,
+								},
+								{
+									members: { $in: [userData._id] },
+									activation: true,
+								},
+							],
+						},
+					],
+				},
+			},
+			{
+				$project: {
+					_id: 1,
+					partner: {
+						_id: 1,
+						name: 1,
+						avatar: {
+							path: 1,
+						},
+						bio: 1,
+					},
+					book: {
+						_id: 1,
+						name: 1,
+						cover: {
+							path: 1,
+						},
+					},
+					lastMessage: {
+						_id: 1,
+						content: 1,
+						sender: {
+							_id: 1,
+							name: 1,
+							avatar: {
+								path: 1,
+							},
+							bio: 1,
+						},
+						room: 1,
+						reader: 1,
+					},
+					activation: 1,
+					creator: 1,
+					roomId: 1,
+					members: 1,
+				},
+			},
+		]);
 
 		console.log(roomData);
-
-		const totalRooms = await Room.countDocuments({
-			$or: [
-				{
-					$and: [
-						{ partner: { $exists: true } },
-						{
-							$lookup: {
-								from: "User", // Assuming your user collection name is 'User'
-								localField: "partner",
-								foreignField: "_id",
-								as: "partnerDetails",
-							},
-						},
-						{
-							$match: {
-								"partnerDetails.name": { $in: keys }, // Case-insensitive search by name
-							},
-						},
-					],
-				},
-				{
-					$and: [
-						{ book: { $exists: true } },
-						{
-							$lookup: {
-								from: "Book", // Assuming your user collection name is 'User'
-								localField: "book",
-								foreignField: "_id",
-								as: "bookDetails",
-							},
-						},
-						{
-							$match: {
-								"bookDetails.name": { $in: keys }, // Case-insensitive search by name
-							},
-						},
-					],
-				},
-			],
-		});
 
 		if (roomData) {
 			socket.emit("get-rooms", {
 				rooms: roomData,
-				totalRooms,
+				// totalRooms,
 				currentPage: Number(page || 1),
-				numberOfPages: Math.ceil(totalRooms / (limit || 10)),
+				// numberOfPages: Math.ceil(totalRooms / (limit || 10)),
 			});
 		} else {
 			socket.emit("error", "This is room not found");
