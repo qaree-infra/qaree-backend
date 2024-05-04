a28ef76a2747fb9779296e9826f3bbad4e8c384b
MOHAMED ALI FAWZI <76850143+MohamedAli00949@users.noreply.github.com>
2024-05-04 15:45:34 +0300

    fix(admin): add get book info query

--- admin/src/graphql/queries/book/book-queries.ts
+++ admin/src/graphql/queries/book/book-queries.ts
@@ -2,10 +2,12 @@ import adminGetBooks from "./getBooks/query.js";
 import getBookEPubMetadata from "./fileContent/getBookEPubMetadata/query.js";
 import getBookEPubManifest from "./fileContent/getBookEPubManifest/query.js";
 import getBookEPubContent from "./fileContent/getBookEPubContent/query.js";
+import getBookInfo from "./getBookInfo/query.js";
 
 export default {
 	adminGetBooks: adminGetBooks,
 	getBookEPubMetadata: getBookEPubMetadata,
 	getBookEPubManifest: getBookEPubManifest,
 	getBookEPubContent: getBookEPubContent,
+	getBookInfo: getBookInfo,
 };
---
+++ admin/src/graphql/queries/book/getBookInfo/resolve.ts
@@ -0,0 +1,37 @@
+import { BookInterface } from "../../../../models/book.js";
+// import verifyBook from "../../../middleware/verifyBook.js";
+import verifyBook from "../../../../middleware/general/verifyBook.js";
+
+interface ArgsInterface {
+	bookId: string;
+}
+
+interface VerifyBookInterface {
+	error?: string;
+	bookData: BookInterface;
+}
+
+const getBookInfoResolve = async (_, args: ArgsInterface, context) => {
+	try {
+		const { bookId } = args;
+		const auth = context.auth;
+
+		if (auth.error) throw new Error(auth.error);
+
+		const { error, bookData }: VerifyBookInterface = await verifyBook(
+			bookId,
+			context,
+		);
+
+		if (error) {
+			throw new Error(error);
+		}
+
+		return bookData;
+	} catch (error) {
+		console.log(error);
+		throw new Error(error.message);
+	}
+};
+
+export default getBookInfoResolve;
