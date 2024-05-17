9c0b74b8310c960efb1c59b38301df3b1149a8f4
MOHAMED ALI FAWZI <76850143+MohamedAli00949@users.noreply.github.com>
2024-05-17 11:19:08 +0300

    fix(reader): fix get top authors query

--- reader/src/graphql/queries/user/getTopAuthors/resolve.ts
+++ reader/src/graphql/queries/user/getTopAuthors/resolve.ts
@@ -1,33 +1,92 @@
 import User, { UserInterface } from "../../../../models/user.js";
+import BookRead from "../../../../models/bookRead.js";
 
 const resolve = async (_, args, context) => {
 	try {
 		const { lang } = context.query;
 
 		const page = args.page || 1;
-		const limit = args.limit || 10;
+		const limit = 25;
 
 		const startIndex = (Number(page) - 1) * limit;
 
-		const total = await User.countDocuments({
-			// bookReads: { $gt: [] },
-			books: { $gt: [] },
-			valid: true,
-		});
+		const authors = await BookRead.aggregate([
+			{
+				$lookup: {
+					from: "books",
+					localField: "book",
+					foreignField: "_id",
+					as: "book",
+				},
+			},
+			{
+				$unwind: "$book",
+			},
+			{
+				$match: {
+					"book.status": "published",
+				},
+			},
+			{
+				$group: {
+					_id: "$book.author",
+					totalReads: { $sum: 1 },
+				},
+			},
+			{
+				$sort: {
+					totalReads: -1,
+				},
+			},
+			{
+				$limit: 10,
+			},
+			{
+				$lookup: {
+					from: "users",
+					localField: "_id",
+					foreignField: "_id",
+					as: "author",
+				},
+			},
+			{
+				$unwind: "$author",
+			},
+			{ $skip: startIndex },
+			{
+				$lookup: {
+					from: "files",
+					localField: "author.avatar",
+					foreignField: "_id",
+					as: "author.avatar",
+				},
+			},
+			{
+				$unwind: "$author.avatar",
+			},
+			{
+				$project: {
+					author: {
+						_id: 1,
+						name: 1,
+						avatar: 1,
+						bio: 1,
+					},
+				},
+			},
+		]);
 
-		const authors = await User.find({
-			books: { $gt: [] },
-			valid: true,
-		})
-		.select("-password")
-		// .sort({ bookReads: 1, books: 1, followers: 1, updatedAt: -1 })
-		.limit(limit)
-		.skip(startIndex)
-		.populate("avatar");
+		console.log(authors);
+
+		const total =
+			authors.length > 0 && authors[0].totalAuthors !== undefined
+				? authors[0].totalAuthors
+				: authors.length;
 
 		return {
-			authors,
-			total,
+			authors:
+				authors.length > 0 ? authors.map((author) => author.author) : authors,
+			total: total,
 			currentPage: page,
 			numberOfPages: Math.ceil(total / limit),
 		};
