16e6e0622b781d1d8441463af500b11f065073d3
MOHAMED ALI FAWZI <76850143+MohamedAli00949@users.noreply.github.com>
2024-05-19 11:03:15 +0300

    fix(author): parse href urls at manifiest

--- author/src/utils/helper.ts
+++ author/src/utils/helper.ts
@@ -54,3 +54,34 @@ export function convertKeys<T>(obj) {
 	}
 	return newObj;
 }
+
+export const parseURL = (url: string) => {
+	const symbolMap = {
+		"%": "%25",
+		"@": "%40",
+		" ": "%20",
+		'"': "%22",
+		"'": "%27",
+		",": "%2C",
+		";": "%3B",
+		"<": "%3C",
+		">": "%3E",
+		"[": "%5B",
+		"]": "%5D",
+		"|": "%7C",
+		"}": "%7D",
+		"{": "%7B",
+	};
+
+	const regex = new RegExp("[" + Object.keys(symbolMap).join("") + "]", "g");
+
+	try {
+		const result = url.replace(regex, (match) => symbolMap[match]);
+		console.log(result);
+		return result;
+	} catch (error) {
+		console.error("Error decoding URL:", error, url);
+		// Handle the error (e.g., return a default URL or log a message)
+		return url; // Or return a placeholder value
+	}
+};
